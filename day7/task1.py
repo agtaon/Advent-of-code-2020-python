0cfdbeaf57b974d57d60c17df107bf840672c8c8
Agaton Fransson <agaton.fransson@gmail.com>
2022-06-28 21:52:01 +0200

    Started on day7 task1

---
+++ day7/task1.py
@@ -0,0 +1,49 @@
+import os
+
+bag_dict = {}
+outcome_dict = {}
+
+
+def line_parser(line):
+    words = line.split()
+    bag = "_".join(words[0:2])
+    bag_dict[bag] = []
+    if "no other bags" in line:
+        return
+
+    for i, word_num in enumerate(words):
+        if word_num in '0123456789':
+            inner_bag = "_".join(words[i+1:i+3])
+            bag_dict[bag] += [inner_bag]
+
+
+def dict_parser(key):
+    values = bag_dict[key]
+
+    if "shiny_gold" in values:
+        outcome_dict[key] = "Yes"
+        return "Yes"
+
+    for value in values:
+        response = dict_parser(value)
+        outcome_dict[key] = response
+
+    return "No"
+
+
+with open(os.path.join(os.getcwd(), "input.txt")) as f:
+    rules = f.readlines()
+    for rule in rules:
+        line_parser(rule)
+
+for key in bag_dict.keys():
+    dict_parser(key)
+
+counter = 0
+for value in outcome_dict.values():
+    if value == "Yes":
+        counter += 1
+
+print(counter)
+# print(outcome_dict, "\n")
+# print(bag_dict, "\n")
