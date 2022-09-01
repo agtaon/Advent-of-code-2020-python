eb3a43def1760e4ce47060fdc55c296190882a36
Agaton Fransson <agfrxa@syntronic.com>
2022-09-01 13:24:21 +0200

    Solve day8 task1

---
+++ day8/task1.py
@@ -0,0 +1,36 @@
+import os
+
+    
+
+with open(os.path.join(os.getcwd(), "input.txt")) as f:
+    instructions = f.readlines()
+    current_index = 0
+    current_value = 0
+    iterator = 0
+    instruction = 0
+    while instructions[current_index] != 'used':
+        iterator += 1
+        if iterator > 10000:
+            print('loop reached limit')
+            break
+        instruction = instructions[current_index].rstrip()
+        instructions[current_index] = 'used'
+        if instruction[0:3] == 'jmp':
+            if instruction[4] == '+':
+                current_index += int(instruction[5:])
+            else:
+                current_index -= int(instruction[5:])
+
+        elif instruction[0:3] == 'acc':
+            current_index += 1
+            if instruction[4] == '+':
+                current_value += int(instruction[5:])
+            else:
+                current_value -= int(instruction[5:])
+
+
+        else:
+            current_index += 1
+        
+    
+    print('iterator: ', iterator, 'current_value: ', current_value, 'current_index: ', current_index)
