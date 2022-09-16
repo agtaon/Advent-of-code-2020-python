674e009e9dc6fc9c72f7737d8305facdd4199c34
Agaton Fransson <agfrxa@syntronic.com>
2022-09-16 15:45:23 +0200

    finish day8 task2

--- day8/task2.py
+++ day8/task2.py
@@ -7,49 +7,49 @@ instruction will be replaced by 'used' hindering an infinite loop.
 
 import os
 
-def instruction_reader_and_changer():
-    current_index = 0
-    current_value = 0
-    iterator = 0
-    instruction = 0
-    while instructions[current_index] != 'used':
-        iterator += 1
-        if iterator > 10000:
-            print('loop reached limit')
-            break
-        instruction = instructions[current_index].rstrip()
-        instructions[current_index] = 'used'
-        if instruction[0:3] == 'jmp':
-            if instruction[4] == '+':
-                current_index += int(instruction[5:])
-            else:
-                current_index -= int(instruction[5:])
-
-        elif instruction[0:3] == 'acc':
-            current_index += 1
-            if instruction[4] == '+':
-                current_value += int(instruction[5:])
-            else:
-                current_value -= int(instruction[5:])
-
-        else:
-            current_index += 1
-        if current_index > len(instructions)-1:
-            break
-    return instruction, current_index, current_value
-
-
 with open(os.path.join(os.getcwd(), "input.txt")) as f:
     instructions = f.readlines()
-    current_index = 0
-    current_value = 0
-    iterator = 0
-    instruction = 0
-    while current_index != len(instructions)-1:
-        instruction, current_index, current_value = instruction_reader_and_changer()
-        if instructions[current_index][0:3] == "jmp":
-            instructions[current_index].replace("jmp", "nop")
+    for i in range (len(instructions)):
+        current_index = 0
+        current_value = 0
+        iterator = 0
+        instruction = 0
+        status = 0
+
+        instructions_copy = instructions.copy()
+        if 'jmp' in instructions_copy[i]:
+            status = 1
+            instructions_copy[i] = instructions_copy[i].replace('jmp', 'nop')
+        
+        elif 'nop' in instructions_copy[i] and status == 0:
+            instructions_copy[i] = instructions_copy[i].replace('nop', 'jmp')
         else:
-            instructions[current_index].replace("nop", "jmp")
-    
-    print('current_value: ', current_value)
+            continue
+        
+        while instructions_copy[current_index] != 'used':
+            iterator += 1
+            if iterator > 10000:
+                print('loop reached limit')
+                break
+            instruction = instructions_copy[current_index].rstrip()
+            instructions_copy[current_index] = 'used'
+            if instruction[0:3] == 'jmp':
+                if instruction[4] == '+':
+                    current_index += int(instruction[5:])
+                else:
+                    current_index -= int(instruction[5:])
+
+            elif instruction[0:3] == 'acc':
+                current_index += 1
+                if instruction[4] == '+':
+                    current_value += int(instruction[5:])
+                else:
+                    current_value -= int(instruction[5:])
+
+            else:
+                current_index += 1
+            if instructions_copy[-1] == 'used':
+                print('current_value: ', current_value)
+                break
+
+        
